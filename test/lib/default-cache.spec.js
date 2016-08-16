99700065da074c8a612b383a00e8aacf517c577e
Nathan Sculli <scull7@gmail.com>
2016-08-16 10:47:38 -0700

    moar tests
    - verify cache.clear() functionality
    - verify synchronous call functionality

--- test/lib/default-cache.spec.js
+++ test/lib/default-cache.spec.js
@@ -87,4 +87,44 @@ describe('default-cache', function() {
 
   });
 
+
+  it('should allow the user to clear the cache.', () => {
+
+    const k1 = 't1';
+    const k2 = 't2';
+
+    const set = (k, v) => new Promise((res, rej) =>
+      cache.set(k, v, (err, val) => err ? rej(err) : res(val))
+    );
+
+    const get = (k) => new Promise((res, rej) =>
+        cache.get(k, (err, val) => err ? rej(err) : res(val))
+    );
+
+    const flush = () => new Promise((res) => cache.clear(res));
+
+
+    return Promise.all([ set(k1, 'foo'), set(k2, 'bar') ])
+
+    .then(flush)
+
+    .then(() => Promise.all([ get(k1), get(k2) ]))
+
+    .then(([ v1, v2 ]) => {
+      expect(v1).to.be.undefined;
+      expect(v2).to.be.undefined;
+    })
+
+  });
+
+
+  it('should run synchronously when not provided a callback', () => {
+
+    cache.set('test', 'sync');
+    const actual = cache.get('test');
+
+    expect(actual).to.eql('sync');
+
+  });
+
 });
